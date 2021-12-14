6c5a00e7671bb916b99cb780837909459413f542
pashwiniis <pashwini684@gmail.com>
2021-12-14 17:20:04 +0530

    User Need Valid First Name

---
+++ Patterns.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexPattern
+{
+        class Patterns
+        {
+            public static string Regex_UserRegestration = "^[A-Z]{1}[a-z]{3,}$";
+           
+            public bool validateFirstName(string FirstName)
+            {
+                return Regex.IsMatch(FirstName, Regex_UserRegestration);
+            }
+        }
+    
+}
