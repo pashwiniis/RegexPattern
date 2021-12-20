cdb7d68739ff01c2dc096021f6530670afbae1cf
pashwiniis <pashwini684@gmail.com>
2021-12-20 22:08:27 +0530

    Password contain at least one upper case

--- Patterns.cs
+++ Patterns.cs
@@ -13,7 +13,7 @@ namespace RegexPattern
              public static string Regex_LastName = "^[A-Z]{1}[a-z]{3,}$";
             public static string Regex_EmailId  = "^[a-z0-9]+(.[a-z0-9]+)?@[a-z]+[.][a-z]{2,3}(.[a-z]{2})?$";
             public static string Regex_MobileFormatt = "^[1-9][0-9]{1,2}[ ][0-9]{10}$";
-            public static string Regex_Password = "[a-zA-Z0-9]{8,}$";
+            public static string Regex_Password = "(?=.*[A-Z])[a-zA-Z0-9]{8,}$";
         public bool validateFirstName(string FirstName)
             {
                 return Regex.IsMatch(FirstName, Regex_FirstName);
